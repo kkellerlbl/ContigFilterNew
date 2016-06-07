80cf1738ec5743de0d4602ccc35478cdbe9a6039
kkellerlbl <kkeller@lbl.gov>
2016-06-07 12:12:06 -0700

    Update run_async.sh

--- scripts/run_async.sh
+++ scripts/run_async.sh
@@ -3,7 +3,7 @@ export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
 WD=/kb/module/work
 if [ -f $WD/token ]; then
     env
-    cat $WD/token | xargs sh $script_dir/../bin/run_MsneddonContigFilter_async_job.sh $WD/input.json $WD/output.json
+#    cat $WD/token | xargs sh $script_dir/../bin/run_MsneddonContigFilter_async_job.sh $WD/input.json $WD/output.json
 else
     echo "File $WD/token doesn't exist, aborting."
     exit 1
